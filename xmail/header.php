51866a9d613ca000f54afc4f08eec87d0976c651
Vaughan Montgomery <vaughan@impresscms.org>
2008-09-30 14:45:00 +0000

    changed references to class/xoopsmodule.php, now refers to kernel/module.php
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/xicletter/branches/2.53@5265 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- xmail/header.php
+++ xmail/header.php
@@ -3,7 +3,7 @@
 * $Id: header.php
 * Module: XMAIL
 * Version: v2.0
-* Release Date: 18 Março 2005
+* Release Date: 18 Marï¿½o 2005
 * Author: Claudia Antonini Vitiello Callegari   Gilberto G. de Oliveira (Giba) 
 * License: GNU
 */
@@ -14,7 +14,7 @@ require_once( XOOPS_ROOT_PATH . "/header.php" );
 require_once XOOPS_ROOT_PATH."/modules/xmail/include/functions.php";
 require_once XOOPS_ROOT_PATH."/class/xoopstree.php";
 require XOOPS_ROOT_PATH."/class/xoopsformloader.php";
-require_once XOOPS_ROOT_PATH."/class/xoopsmodule.php";
+require_once XOOPS_ROOT_PATH.'/kernel/module.php';
 
 require_once XOOPS_ROOT_PATH."/modules/xmail/include/classparam.php";
 
@@ -34,10 +34,10 @@ if (!$xoopsUser ) {
 	exit();
 }
 
-// checar se user é admin
+// checar se user ï¿½ admin
 //	$xoopsModule = XoopsModule::getByDirname($modversion['dirname']);
-// acima não deu certo pois não reconhece $modversion  -  verificar qual melhor forma de fazer
-//  sem colocar explicitamente  xmail  e sim resgatar com função
+// acima nï¿½o deu certo pois nï¿½o reconhece $modversion  -  verificar qual melhor forma de fazer
+//  sem colocar explicitamente  xmail  e sim resgatar com funï¿½ï¿½o
 //  Couldn't make work as above since $modversion isn't recognized. To do: check the
 //  best way to set $xoopsModule without the need to input "xmail", i.e., fetch it with
 //  a function as in the commented line above.
